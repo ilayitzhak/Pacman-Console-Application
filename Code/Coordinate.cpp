cc64941667d033dd56ff8974c55b678cf25378aa
ilayitzhak <ilayy1324@gmail.com>
2023-10-23 14:41:35 +0300

    Remove everything

--- Code/Coordinate.cpp
+++
@@ -1,6 +0,0 @@
-#include "Coordinate.h"
-
-bool Coordinate::compareCoords(const Coordinate& coordToCompare) const
-{
-	return m_x == coordToCompare.getCoordX() && m_y == coordToCompare.getCoordY();
-}
